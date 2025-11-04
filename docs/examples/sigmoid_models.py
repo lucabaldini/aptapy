dcefa59fb90b800c9f75b31561e39037a152011a
Luca Baldini <luca.baldini@pi.infn.it>
2025-11-04 06:25:27 +0100

    Initial import.

---
+++ docs/examples/sigmoid_models.py
@@ -0,0 +1,19 @@
+"""
+Peak models
+===========
+
+Illustration of the available sigmoid models with the standard location and scale
+parameters
+"""
+
+# %%
+
+from aptapy.plotting import plt
+from aptapy.models import Erf, Logistic, Arctangent, HyperbolicTangent
+
+Erf().plot()
+Logistic().plot()
+Arctangent().plot()
+HyperbolicTangent().plot()
+
+plt.legend()

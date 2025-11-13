b74d00162350d4dea035c078d71965a11b23b47f
Luca Baldini <luca.baldini@pi.infn.it>
2025-11-13 06:11:23 +0100

    Minor.

--- docs/examples/exponential_fit.py
+++ docs/examples/exponential_fit.py
@@ -2,7 +2,7 @@
 Exponential fit
 ===============
 
-
+Fit some real data with exponential and stretched exponential models.
 """
 
 # %%
@@ -15,15 +15,19 @@ from aptapy.models import Constant, Exponential, StretchedExponential
 from aptapy.plotting import setup_axes, residual_axes
 
 
+# These are real data recorded with a thermistor as it cools down in open air.
 file_path = pathlib.Path.cwd() / "data" / "exponential_data.txt"
 t, T = np.loadtxt(file_path, unpack=True)
 
+# Setup two different models: a simple exponential + constant (Newton law)
+# and a stretched exponential + constant.
 simple_model = Constant() + Exponential()
 simple_model.fit(t, T, sigma=0.1)
 
 stretched_model = Constant() + StretchedExponential()
 stretched_model.fit(t, T, sigma=0.1)
 
+# The fit and compare the two---the latter is clearly better.
 _, ax1, ax2 = residual_axes(height_ratio=0.3)
 ax1.errorbar(t, T, fmt="o", label="Data", alpha=0.75, ms=5., color="gray")
 simple_model.plot(ax1, fit_output=True, plot_components=False, zorder=10)

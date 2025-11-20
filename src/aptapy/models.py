e68f43756e94e04fbb18175612a91b446522fae0
Luca Baldini <luca.baldini@pi.infn.it>
2025-11-20 21:50:20 +0100

    Fixed order of the coefficients.

--- src/aptapy/models.py
+++ src/aptapy/models.py
@@ -212,9 +212,8 @@ class Polynomial(AbstractFitModel):
     def evaluate(x: ArrayLike, *coefficients: float) -> ArrayLike:
         # pylint: disable=arguments-differ
         result = np.zeros_like(x)
-        degree = len(coefficients) - 1
         for i, c in enumerate(coefficients):
-            result += c * x**(degree - i)
+            result += c * x**i
         return result
 
     def init_parameters(self, xdata: ArrayLike, ydata: ArrayLike, sigma: ArrayLike = 1.) -> None:

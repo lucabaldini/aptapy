e459e9e93d4da91897c7b6bff06ec3c4d4db74f4
Luca Baldini <luca.baldini@pi.infn.it>
2025-11-24 17:14:33 +0100

    Minor.

--- docs/examples/gaussian_forest.py
+++ docs/examples/gaussian_forest.py
@@ -2,8 +2,7 @@
 Gaussian forest
 ===============
 
-Definition of a GaussianForest child class and creation and fit of a histogram from the
-given model.
+Illustration of the GaussianForest class and the line_forest decorator.
 """
 
 # %%
@@ -14,12 +13,14 @@ from aptapy.plotting import plt
 
 @line_forest(1., 3.)
 class ExampleForest(GaussianForest):
-    """Example of a GaussianForest child class with lines centeres in 1. and 2. [a.u.]
+    """Example of a GaussianForest child class with lines centered at 1. and 3. [a.u.]
     """
 
 # Instantiate the class and initialize the parameters
 model = ExampleForest()
 model.amplitude1.init(0.1)
+model.sigma.init(0.75)
+
 # Generate a random histogram with the given parameters
 hist = model.random_histogram(size=100000, num_bins=100)
 hist.label = "Random data"

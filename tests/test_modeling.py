5c13ac517d488c48b98aecaaca3239299e9c8a68
Luca Baldini <luca.baldini@pi.infn.it>
2025-12-17 10:49:54 +0100

    Minor.

--- tests/test_modeling.py
+++ tests/test_modeling.py
@@ -354,6 +354,6 @@ def test_random_histogram():
     edges = np.linspace(model.mu.value - 3 * model.sigma.value,
                          model.mu.value + 3 * model.sigma.value, 101)
     hist = model.random_histogram(edges=edges, size=100000, random_state=_RNG)
-    hist.plot()
+    hist.plot(label="Random data")
     plt.legend()
     assert np.array_equal(hist.bin_edges(), edges)
